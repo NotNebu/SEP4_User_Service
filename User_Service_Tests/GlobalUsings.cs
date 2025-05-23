3fa5826d169fdb7723bacaefbbeaa43cb38d3355
Ditteoc <Ditte.overgaard@hotmail.com>
2025-05-23 19:47:44 +0200

    actions build fix, fjernet grpc dependencies

--- User_Service_Tests/GlobalUsings.cs
+++ User_Service_Tests/GlobalUsings.cs
@@ -1,5 +1,3 @@
 global using SEP4_User_Service.Domain.Entities;
 global using SEP4_User_Service.Application.UseCases;
-global using Protos.Grpc;
-global using Grpc.Core.Testing;
 global using Xunit;
